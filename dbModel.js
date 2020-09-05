2b10baac0d7fd9c5cb677644489935da29d6c0cb
Bipin Karki <bipinkarki172@gmail.com>
2020-09-05 11:10:41 +0545

    Heroku code update

--- dbModel.js
+++ dbModel.js
@@ -1,15 +1,16 @@
-const mongoose = require("mongoose");
+// const mongoose = require("mongoose");
+import mongoose from "mongoose";
 
 const tiktokSchema = mongoose.Schema({
-    url: String,
-    channel: String,
-    song: String,
-    likes: String,
-    messages: String,
-    description: String,
-    shares: String,
+  url: String,
+  channel: String,
+  song: String,
+  likes: String,
+  messages: String,
+  description: String,
+  shares: String,
 });
 
 //  collection into database
 // module.exports =  mongoose.model('tiktokVideos', tiktokSchema);
-export default mongoose.model('tiktokVideos', tiktokSchema);
+export default mongoose.model("tiktokVideos", tiktokSchema);
